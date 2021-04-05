465b006aa9eb7c92ee435f957364e0a9667ea12e
fabianko <fabian.arismendi@usach.cl>
2021-04-05 19:31:46 -0400

    ADD: componentes principales para tic tac toe

--- tic-tac-toe-angular/src/app/app.module.ts
+++ tic-tac-toe-angular/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { CellComponent } from './board/cell/cell.component';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    CellComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule
